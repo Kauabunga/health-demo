3a9bc3e935f6fce5ac505b82ed6a0bb4be7b6df6
Carson Bruce <carson.bruce@gmail.com>
2019-07-16 08:54:53 +1200

    added sentry

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Router from "./Router";
 import * as serviceWorker from "./serviceWorker";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({ dsn: "https://0829c7b4eaca469aa415d5fc078311ba@sentry.io/1505467" });
 
 ReactDOM.render(<Router />, document.getElementById("root"));
 
