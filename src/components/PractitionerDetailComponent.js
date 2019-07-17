ca9c78832e0f36f7a8cd48b056c00f0bb0383da0
Carson Bruce <carson.bruce@gmail.com>
2019-07-18 11:40:11 +1200

    beginning of practitioner screen

--- src/components/PractitionerDetailComponent.js
+++ src/components/PractitionerDetailComponent.js
@@ -1,142 +1,52 @@
-import React, { Fragment } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Grid from "@material-ui/core/Grid";
-import Avatar from "@material-ui/core/Avatar";
-import Card from "@material-ui/core/Card";
-import Divider from "@material-ui/core/Divider";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { red } from "@material-ui/core/colors";
-import IconButton from "@material-ui/core/IconButton";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
 
-import StandardLayout from "../layout/StandardLayout";
-import DelayComponent from "./DelayComponent";
-import PatientConditionContainer from "../containers/PatientConditionContainer";
+import StandardLayout from '../layout/StandardLayout';
+import DelayComponent from './DelayComponent';
 
 const rootStyles = theme => ({
   columnContainer: {
-    flexWrap: "wrap",
-    [theme.breakpoints.up("md")]: {
-      flexWrap: "nowrap"
+    flexWrap: 'wrap',
+    [theme.breakpoints.up('md')]: {
+      flexWrap: 'nowrap'
     }
   }
 });
-
-const useExpansionPanelStyles = makeStyles(theme => ({
-  root: {
-    boxShadow: "none"
-  }
-}));
-const useExpansionPanelSummaryStyles = makeStyles(theme => ({
-  root: {
-    padding: 0
-  }
-}));
 
 const DetailItem = ({ label, value, noWrap }) => (
   <Grid item>
     <Typography>{label}</Typography>
     <Typography variant="subtitle1" style={{ fontWeight: 700 }} noWrap={noWrap}>
-      {value || "unknown"}
+      {value || 'unknown'}
     </Typography>
   </Grid>
 );
 
-const DetailGroup = ({ label, items, labelProp, value }) => {
-  const classesExpansionPanel = useExpansionPanelStyles();
-  const classesExpansionPanelSummary = useExpansionPanelSummaryStyles();
-
-  const labelProps = [].concat(labelProp);
-  const labelValue = labelProps.map(prop => value[prop]).join(" - ");
-  return (
-    <Grid item>
-      <ExpansionPanel classes={classesExpansionPanel}>
-        <ExpansionPanelSummary
-          classes={classesExpansionPanelSummary}
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <DetailItem label={label} value={labelValue} />
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Grid container direction="column">
-            {items.map(item => (
-              <Fragment>
-                <DetailItem label={item.label} noWrap={item.noWrap} value={value[item.key]} />
-                <Divider style={{ marginTop: 12, marginBottom: 12 }} />
-              </Fragment>
-            ))}
-          </Grid>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-    </Grid>
-  );
-};
-
-const DetailPatient = ({ currentPatient }) => (
+const DetailPractitioner = ({ currentPractitioner }) => (
   <Grid item style={{ paddingRight: 24 }}>
-    <DetailItem label="Name" value={currentPatient.name} />
+    <DetailItem label="Name" value={currentPractitioner.name} />
     <Divider style={{ marginTop: 12, marginBottom: 12 }} />
-    <DetailItem label="Email" value={currentPatient.email} noWrap />
+    <DetailItem label="Email" value={currentPractitioner.email} noWrap />
     <Divider style={{ marginTop: 12, marginBottom: 12 }} />
-    <DetailItem label="Address" value={currentPatient.address} />
+    <DetailItem label="Address" value={currentPractitioner.address} />
     <Divider style={{ marginTop: 12, marginBottom: 12 }} />
-    <DetailItem label="Birth date" value={currentPatient.birthDate} />
-    <Divider style={{ marginTop: 12, marginBottom: 12 }} />
-    <DetailItem label="Gender" value={currentPatient.gender} />
-    <Divider style={{ marginTop: 12, marginBottom: 12 }} />
-    <DetailItem label="Marital status" value={currentPatient.maritalStatus} />
-    <Divider style={{ marginTop: 12, marginBottom: 12 }} />
-    <DetailItem label="Ethnicity" value={currentPatient.ethnicity} />
-    <Divider style={{ marginTop: 12, marginBottom: 12 }} />
-    <DetailItem label="Care level" value={currentPatient.carePlan} />
-    <Divider style={{ marginTop: 12, marginBottom: 12 }} />
-    {(currentPatient.nextOfKins || []).map(nextOfKin => (
-      <DetailGroup
-        key={nextOfKin && nextOfKin.name}
-        label="Contact"
-        labelProp={["name"]}
-        items={[
-          { label: "Name", key: "name" },
-          { label: "Phone", key: "phone", noWrap: true },
-          { label: "Email", key: "email" },
-          { label: "Gender", key: "gender" },
-          { label: "Relationship", key: "relationship" }
-        ]}
-        value={nextOfKin}
-      />
-    ))}
   </Grid>
 );
 
-const DetailDiagnoses = ({ notes }) => {
-  return (
-    <Grid item style={{ flexGrow: 1, flexShrink: 0, maxWidth: 380 }}>
-      <Typography variant="h6">Conditions</Typography>
-      {(notes || []).map(({ id, text }) => (
-        <Grid key={id || text} style={{ marginBottom: 12, padding: 12 }}>
-          <Typography>{text || "unknown"}</Typography>
-        </Grid>
-      ))}
-    </Grid>
-  );
-};
+function PractitionerDetailComponent(props) {
+  const { classes, practitionerId, currentPractitioner, currentPractitionerLoading, currentPractitionerError } = props;
 
-function PatientDetailComponent(props) {
-  const { classes, patientId, currentPatient, currentPatientLoading, currentPatientError } = props;
-
-  if (currentPatientLoading) {
+  if (currentPractitionerLoading) {
     return (
       <Grid container alignItems="center" justify="center">
         <DelayComponent wait={100}>
@@ -148,50 +58,30 @@ function PatientDetailComponent(props) {
 
   return (
     <StandardLayout>
-      {currentPatient && (
+      {currentPractitioner && (
         <Grid container direction="row" alignItems="center" style={{ marginBottom: 12 }}>
-          {(currentPatient && currentPatient.photo && (
-            <Avatar alt={currentPatient.name} src={currentPatient.photo} />
-          )) || <Avatar>{currentPatient.initials}</Avatar>}
+          {(currentPractitioner && currentPractitioner.photo && (
+            <Avatar alt={currentPractitioner.name} src={currentPractitioner.photo} />
+          )) || <Avatar>{currentPractitioner.initials}</Avatar>}
           <Typography variant="h6" style={{ marginLeft: 8 }}>
-            {currentPatient.name} (Patient {patientId})
+            {currentPractitioner.name} (Practitioner {practitionerId})
           </Typography>
 
           <div style={{ flexGrow: 1 }}></div>
-
-          <Button
-            style={{ marginRight: 12 }}
-            color="primary"
-            variant="outlined"
-            component={Link}
-            to={`/patient/${patientId}/notes`}
-          >
-            Add Consultation Note
-          </Button>
-
-          <LongMenu patientId={patientId} />
         </Grid>
       )}
 
-      {currentPatient && (
+      {currentPractitioner && (
         <Card style={{ padding: 24 }}>
           <Grid container direction="row" className={classes.columnContainer}>
-            <DetailPatient currentPatient={currentPatient} />
-
-            <PatientConditionContainer
-              patientId={patientId}
-              Layout={({ currentPatientCondition }) => {
-                const { notes } = currentPatientCondition || {};
-                return (currentPatientCondition && <DetailDiagnoses notes={notes} />) || <CircularProgress />;
-              }}
-            />
+            <DetailPractitioner currentPractitioner={currentPractitioner} />
           </Grid>
         </Card>
       )}
 
-      {currentPatientError && (
-        <pre style={{ color: red[500], width: "100%", overflowX: "scroll" }}>
-          {JSON.stringify(currentPatientError, null, 2)}
+      {currentPractitionerError && (
+        <pre style={{ color: red[500], width: '100%', overflowX: 'scroll' }}>
+          {JSON.stringify(currentPractitionerError, null, 2)}
         </pre>
       )}
       <Button component={Link} to="/" variant="outlined" color="primary" style={{ marginTop: 12 }}>
@@ -201,51 +91,8 @@ function PatientDetailComponent(props) {
   );
 }
 
-PatientDetailComponent.propTypes = {
+PractitionerDetailComponent.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(rootStyles)(PatientDetailComponent);
-
-function LongMenu({ patientId }) {
-  const ITEM_HEIGHT = 48;
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-
-  function handleClick(event) {
-    setAnchorEl(event.currentTarget);
-  }
-
-  function handleClose() {
-    setAnchorEl(null);
-  }
-
-  return (
-    <div>
-      <IconButton aria-label="More" aria-controls="long-menu" aria-haspopup="true" onClick={handleClick}>
-        <MoreVertIcon />
-      </IconButton>
-      <Menu
-        id="long-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          style: {
-            maxHeight: ITEM_HEIGHT * 4.5,
-            width: 240
-          }
-        }}
-      >
-        <MenuItem component={Link} to={`/patient/${patientId}/transfer-pack`} onClick={handleClose}>
-          Create Transfer Pack
-        </MenuItem>
-        <MenuItem component={Link} to={`/patient/${patientId}/discharge`} onClick={handleClose}>
-          Create Discharge Summary
-        </MenuItem>
-      </Menu>
-    </div>
-  );
-}
+export default withStyles(rootStyles)(PractitionerDetailComponent);
--- src/state/PractitionerState.js
+++ src/state/PractitionerState.js
@@ -47,7 +47,7 @@ export const practitionerStore = new decorated();
 export default React.createContext(practitionerStore);
 
 async function getPractitioner(practitionerId) {
-  const { client_id, base_uri, base_path_practitioner, errorsPractitioner } = credentialsStore;
+  const { client_id, base_uri, base_path_practitioner } = credentialsStore;
   const { session } = authStore;
   const { id_token } = session || {};
 
@@ -69,15 +69,9 @@ async function getPractitioner(practitionerId) {
 
     return transformPractitioner(data);
   } catch (error) {
-    console.error('Error getting practitioner', errorsPractitioner, practitionerId, error);
+    console.error('Error getting practitioner', practitionerId, error);
 
-    // If errors are enabled
-    if (errorsPractitioner) {
-      throw error;
-    }
-
-    await wait(getRandomInt(400, 2500));
-    return transformPractitioner(getDummyPractitioner());
+    throw error;
   }
 }
 
@@ -90,25 +84,26 @@ export function transformPractitioner(practitioner) {
   const photo = firstPhotoData && `data:image/png;base64,${firstPhotoData}`;
 
   // EMAIL
-  const { value: email } = telecom.find(({ system }) => system === 'email') || {};
+  const { value: email } = (telecom || []).find(({ system }) => system === 'email') || {};
 
   // NAME
-  const officialName = name.find(({ use }) => use === 'official');
-  const fullOfficialName = officialName.given.concat(officialName.family).join(' ');
+  const officialName = (name || []).find(({ use }) => use === 'official');
+  const fullOfficialName = officialName && officialName.given.concat(officialName.family).join(' ');
 
   // ADDRESS
-  const { line } = (address && address[0]) || {};
+  const { line, text: addressText } = (address && address[0]) || {};
   const prettyAddress =
-    line &&
-    line
-      .filter(l => !!l)
-      .join(', ')
-      .replace(/^,/gi, '')
-      .trim();
+    (line &&
+      line
+        .filter(l => !!l)
+        .join(', ')
+        .replace(/^,/gi, '')
+        .trim()) ||
+    addressText;
 
   // INITIALS
-  const initials = fullOfficialName.match(/\b\w/g) || [];
-  const formattedInitials = ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+  const initials = (fullOfficialName && fullOfficialName.match(/\b\w/g)) || [];
+  const formattedInitials = initials && ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
 
   // NEXT OF KIN
   const nextOfKinContact = contact;
@@ -129,13 +124,10 @@ export function transformPractitioner(practitioner) {
   });
 
   // EXTENSIONS
-  const carePlanExtension = extension.find(({ url }) => url && url.includes('CareLevel'));
+  const carePlanExtension = (extension || []).find(({ url }) => url && url.includes('CareLevel'));
   const carePlan = carePlanExtension && carePlanExtension.valueCoding.display;
-  const ethnicityExtension = extension.find(({ url }) => url && url.includes('Ethnicities'));
+  const ethnicityExtension = (extension || []).find(({ url }) => url && url.includes('Ethnicities'));
   const ethnicity = ethnicityExtension && ethnicityExtension.valueCoding.display;
-
-  // MARITAL STATUS
-  const maritalStatus = practitioner.maritalStatus && practitioner.maritalStatus.text;
 
   const officialIdentifier = (practitioner.identifier || []).find(({ use }) => use === 'official');
   const secondaryIdentifier = (practitioner.identifier || []).find(({ use }) => use === 'secondary');
@@ -153,323 +145,10 @@ export function transformPractitioner(practitioner) {
     gender,
 
     carePlan,
-    maritalStatus,
     ethnicity,
 
     nextOfKins,
 
     __original__: practitioner
   };
-}
-
-export function getDummyPractitioner() {
-  return {
-    resourceType: 'Practitioner',
-    id: '2143.19',
-    meta: {
-      versionId: '6'
-    },
-    extension: [
-      {
-        url: 'http://apac-syd-partner02-test.apigee.net/fhir4-0-0/Extensions/Practitioner/CareLevel',
-        valueCoding: {
-          system: 'http://rymanhealthcare.co.nz',
-          code: 'R-D1',
-          display: 'Dementia - Respite'
-        }
-      },
-      {
-        url: 'http://apac-syd-partner02-test.apigee.net/fhir4-0-0/Extensions/Practitioner/Ethnicities',
-        valueCoding: {
-          system: 'http://rymanhealthcare.co.nz',
-          code: '11',
-          display: 'NZ European'
-        }
-      }
-    ],
-    identifier: [
-      {
-        use: 'official',
-        system: 'http://health.govt.nz/nhi',
-        value: 'ZBP8438'
-      },
-      {
-        use: 'secondary',
-        system: 'http://rymanhealthcare.co.nz',
-        value: '2143.19'
-      }
-    ],
-    name: [
-      {
-        use: 'official',
-        family: 'Roberson',
-        given: ['Kloe'],
-        prefix: ['Mrs']
-      },
-      {
-        use: 'usual',
-        given: ['Kloe']
-      }
-    ],
-    telecom: [
-      {
-        system: 'email',
-        value: 'Kloe.Roberson@residents.AdaLovelace.testvillages.rymanhealthcare.com',
-        use: 'home',
-        period: {
-          start: '2018-04-04T00:00:00+00:00',
-          end: '1900-01-01T00:00:00+00:00'
-        }
-      },
-      {
-        system: 'phone',
-        value: 'tel:-1-3334293',
-        use: 'home',
-        period: {
-          start: '2018-04-04T00:00:00+00:00',
-          end: '1900-01-01T00:00:00+00:00'
-        }
-      },
-      {
-        system: 'phone',
-        value: 'tel:-5-246433;ext="',
-        use: 'home',
-        period: {
-          start: '2018-04-04T00:00:00+00:00',
-          end: '1900-01-01T00:00:00+00:00'
-        }
-      }
-    ],
-    gender: 'unknown',
-    birthDate: '1916-02-02',
-    address: [
-      {
-        use: 'home',
-        type: 'physical',
-        text: 'D021,Dementia,Ada Lovelace,OTHER',
-        line: ['D021', 'Dementia', 'Ada Lovelace', '', '', '', '', '', 'OTHER'],
-        city: '',
-        postalCode: '',
-        country: 'OTHER',
-        period: {
-          start: '2018-04-04T00:00:00+00:00',
-          end: '1900-01-01T00:00:00+00:00'
-        }
-      }
-    ],
-    maritalStatus: {
-      coding: [
-        {
-          system: 'http://terminology.hl7.org/CodeSystem/v3-MaritalStatus',
-          code: 'W',
-          display: 'Widowed'
-        }
-      ],
-      text: 'Widowed'
-    },
-    photo: [
-      {
-        contentType: 'image/jpeg',
-        data: '',
-        size: 0,
-        hash: '2jmj7l5rSw0yVb/vlWAYkK/YBwk=',
-        title: 'Small picture'
-      },
-      {
-        contentType: 'image/jpeg',
-        data: '',
-        size: 0,
-        hash: '2jmj7l5rSw0yVb/vlWAYkK/YBwk=',
-        title: 'Large picture'
-      }
-    ],
-    contact: [
-      {
-        relationship: [
-          {
-            coding: [
-              {
-                system: 'http://terminology.hl7.org/CodeSystem/v2-0131',
-                code: 'N',
-                display: 'Next-of-Kin'
-              }
-            ],
-            text: 'First Next-of-Kin (Son)'
-          }
-        ],
-        name: {
-          use: 'official',
-          family: 'Mcfadden',
-          given: ['Charles']
-        },
-        telecom: [
-          {
-            system: 'email',
-            value: 'Charles.Mcfadden@nok.AdaLovelace.testvillages.rymanhealthcare.com',
-            use: 'home',
-            period: {
-              start: '2019-05-27T00:00:00+00:00',
-              end: '1900-01-01T00:00:00+00:00'
-            }
-          },
-          {
-            system: 'other',
-            value: 'PHYSICAL',
-            use: 'home',
-            period: {
-              start: '2019-05-27T00:00:00+00:00',
-              end: '1900-01-01T00:00:00+00:00'
-            }
-          }
-        ],
-        gender: 'male',
-        period: {
-          start: '2000-01-01T00:00:00+00:00',
-          end: '1900-01-01T00:00:00+00:00'
-        }
-      },
-      {
-        relationship: [
-          {
-            text: 'Pending Enduring Power of Attorney'
-          }
-        ],
-        name: {
-          use: 'official',
-          family: 'Mcfadden',
-          given: ['Charles']
-        },
-        telecom: [
-          {
-            system: 'email',
-            value: 'Charles.Mcfadden@nok.AdaLovelace.testvillages.rymanhealthcare.com',
-            use: 'home',
-            period: {
-              start: '2019-05-27T00:00:00+00:00',
-              end: '1900-01-01T00:00:00+00:00'
-            }
-          },
-          {
-            system: 'other',
-            value: 'PHYSICAL',
-            use: 'home',
-            period: {
-              start: '2019-05-27T00:00:00+00:00',
-              end: '1900-01-01T00:00:00+00:00'
-            }
-          }
-        ],
-        gender: 'male',
-        period: {
-          start: '2000-01-01T00:00:00+00:00',
-          end: '1900-01-01T00:00:00+00:00'
-        }
-      },
-      {
-        relationship: [
-          {
-            text: 'EPOA Active - Personal Care & Welfare'
-          }
-        ],
-        name: {
-          use: 'official',
-          family: 'Marshall',
-          given: ['Willow']
-        },
-        telecom: [
-          {
-            system: 'email',
-            value: 'Willow.Marshall@nok.AdaLovelace.testvillages.rymanhealthcare.com',
-            use: 'home',
-            period: {
-              start: '2019-05-27T00:00:00+00:00',
-              end: '1900-01-01T00:00:00+00:00'
-            }
-          },
-          {
-            system: 'other',
-            value: 'PHYSICAL',
-            use: 'home',
-            period: {
-              start: '2019-05-27T00:00:00+00:00',
-              end: '1900-01-01T00:00:00+00:00'
-            }
-          }
-        ],
-        gender: 'male',
-        period: {
-          start: '2000-01-01T00:00:00+00:00',
-          end: '1900-01-01T00:00:00+00:00'
-        }
-      },
-      {
-        relationship: [
-          {
-            text: 'Pending Enduring Power of Attorney'
-          }
-        ],
-        name: {
-          use: 'official',
-          family: 'Gill',
-          given: ['Mohsin']
-        },
-        telecom: [
-          {
-            system: 'email',
-            value: 'Mohsin.Gill@nok.AdaLovelace.testvillages.rymanhealthcare.com',
-            use: 'home',
-            period: {
-              start: '2019-05-27T00:00:00+00:00',
-              end: '1900-01-01T00:00:00+00:00'
-            }
-          },
-          {
-            system: 'other',
-            value: 'PHYSICAL',
-            use: 'home',
-            period: {
-              start: '2019-05-27T00:00:00+00:00',
-              end: '1900-01-01T00:00:00+00:00'
-            }
-          }
-        ],
-        gender: 'male',
-        period: {
-          start: '2000-01-01T00:00:00+00:00',
-          end: '1900-01-01T00:00:00+00:00'
-        }
-      }
-    ],
-    communication: [
-      {
-        language: {
-          coding: [
-            {
-              system: 'http://rymanhealthcare.co.nz',
-              code: 'eng',
-              display: 'English'
-            }
-          ],
-          text: 'English'
-        }
-      }
-    ],
-    generalPractitioner: [
-      {
-        reference: 'http://apac-syd-partner02-test.apigee.net/fhir4-0-0/Practitioner/2219.2',
-        type: 'Practitioner',
-        display: 'Arnold Leith'
-      }
-    ]
-  };
-}
-
-async function wait(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms || 500));
-}
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
